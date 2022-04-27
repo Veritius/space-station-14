6c60523cda83c6918feac293fb2cb0f8e21b9341
Veritius <veritiusgaming@gmail.com>
2022-04-27 12:24:13 +1000

    Add component prototype name

--- Content.Server/SerpentidAbilities/Components/HuntingModeComponent.cs
+++ Content.Server/SerpentidAbilities/Components/HuntingModeComponent.cs
@@ -7,6 +7,7 @@ namespace Content.Server.SerpentidAbilities
     ///     Component given to serpentids that represents Hunting and Manipulation modes.
     /// </summary>
     [RegisterComponent]
+    [ComponentProtoName("HuntingMode")]
     public class HuntingModeComponent : Component
     {
         [ViewVariables]
