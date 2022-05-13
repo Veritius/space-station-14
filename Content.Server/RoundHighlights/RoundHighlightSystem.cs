2932be136f71b9e4527e664a188e124aa37507fd
Veritius <veritiusgaming@gmail.com>
2022-05-13 23:24:02 +1000

    Count security disarms

--- Content.Server/RoundHighlights/RoundHighlightSystem.cs
+++ Content.Server/RoundHighlights/RoundHighlightSystem.cs
@@ -1,3 +1,4 @@
+using Content.Server.CombatMode;
 using Content.Server.GameTicking;
 using Content.Server.Weapon.Melee;
 using Content.Shared.CCVar;
@@ -14,6 +15,8 @@ namespace Content.Server.RoundHighlight
         [Dependency] private readonly IConfigurationManager _configurationManager = default!;
 
         public int ClownsBrutalisedCounter; // Goes up when a clown is hit
+        // TODO: This may not make sense in future when/if disarms get reworked
+        public int SecurityDisarmedCounter; // Goes up every time a security officer is disarmed
 
         public override void Initialize()
         {
@@ -21,17 +24,20 @@ namespace Content.Server.RoundHighlight
             SubscribeLocalEvent<RoundRestartCleanupEvent>(Reset);
             SubscribeLocalEvent<RoundEndTextAppendEvent>(OnRoundEnd);
             SubscribeLocalEvent<RoundHighlightTrackerComponent, MeleeHitEvent>(OnMeleeHit);
+            SubscribeLocalEvent<RoundHighlightTrackerComponent, DisarmedEvent>(OnDisarm);
         }
 
         private void Reset(RoundRestartCleanupEvent args)
         {
             ClownsBrutalisedCounter = 0;
+            SecurityDisarmedCounter = 0;
         }
 
         private void OnRoundEnd(RoundEndTextAppendEvent args)
         {
             if (!_configurationManager.GetCVar(CCVars.RoundHighlights)) return;
             args.AddLine(Loc.GetString("round-highlight-clowns-brutalised", ("count", ClownsBrutalisedCounter)));
+            args.AddLine(Loc.GetString("round-highlight-security-disarms", ("count", SecurityDisarmedCounter)));
         }
 
         private void OnMeleeHit(EntityUid uid, RoundHighlightTrackerComponent component, MeleeHitEvent args)
@@ -39,6 +45,14 @@ namespace Content.Server.RoundHighlight
             if (component.OwnerTags.Contains("clown"))
             {
                 ClownsBrutalisedCounter += 1;
+            }
+        }
+
+        private void OnDisarm(EntityUid uid, RoundHighlightTrackerComponent component, DisarmedEvent args)
+        {
+            if (component.OwnerTags.Contains("security"))
+            {
+                SecurityDisarmedCounter += 1;
             }
         }
     }
--- Resources/Locale/en-US/round-end/round-highlights.ftl
+++ Resources/Locale/en-US/round-end/round-highlights.ftl
@@ -3,3 +3,8 @@ round-highlight-clowns-brutalised = The clown was brutalised [color=green]{$coun
 [1] time
 *[2] times
 }.
+round-highlight-security-disarms = Security officers were disarmed [color=green]{$count}[/color] { $count ->
+[0] times
+[1] time
+*[2] times
+}.
