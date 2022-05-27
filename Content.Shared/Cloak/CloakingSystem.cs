1ef93807252452b129b04035745dbe642a8593b4
Veritius <veritiusgaming@gmail.com>
2022-05-27 22:27:54 +1000

    system

--- Content.Shared/Cloak/CloakingSystem.cs
+++ Content.Shared/Cloak/CloakingSystem.cs
@@ -2,6 +2,9 @@ namespace Content.Shared.Cloak
 {
     public sealed class CloakingSystem : EntitySystem
     {
-
+        public override void Initialize()
+        {
+            base.Initialize();
+        }
     }
 }
