d18cb50242ef5c383e801e4f3a4d88f619bd96a0
veritius <veritiusgaming@gmail.com>
2024-09-08 20:10:36 +1000

    save

--- Content.Server/Administration/Managers/AutoMod.cs
+++ Content.Server/Administration/Managers/AutoMod.cs
@@ -1,9 +1,13 @@
+using Robust.Server.Player;
 using Robust.Shared.Player;
 
 namespace Content.Server.Administration.Managers;
 
-public sealed class AutoMod: IAutoMod
+public sealed class AutoMod : IAutoMod
 {
+    [Dependency] private readonly IPlayerManager _playerManager = default!;
+    [Dependency] private readonly IBanManager _banManager = default!;
+
     public void RaiseHeat(ICommonSession session, uint amount)
     {
         throw new NotImplementedException();
@@ -13,4 +17,9 @@ public sealed class AutoMod: IAutoMod
     {
         throw new NotImplementedException();
     }
+}
+
+struct PlayerHeat
+{
+    private uint heat;
 }
